4abddce0581f092eba4fff89768b5b4630cf0b4a
Harry Adel <harryadelb@gmail.com>
2018-10-23 18:52:55 +0200

    Modify jszip require statment

--- dist/ods.js
+++ dist/ods.js
@@ -52,11 +52,11 @@ function getzipdata(zip, file, safe) {
 var _fs, jszip;
 if(typeof JSZip !== 'undefined') jszip = JSZip;
 if (typeof exports !== 'undefined') {
-	if (typeof module !== 'undefined' && module.exports) {
-		if(has_buf && typeof jszip === 'undefined') jszip = require('js'+'zip');
-		if(typeof jszip === 'undefined') jszip = require('./js'+'zip').JSZip;
-		_fs = require('f'+'s');
-	}
+	if(typeof exports !== 'undefined') {
+        if(typeof module !== 'undefined' && module.exports) {
+	           if(typeof jszip === 'undefined') jszip = require('./jszip.js');
+        }
+     }
 }
 var attregexg=/\b[\w:-]+=["'][^"]*['"]/g;
 var tagregex=/<[^>]*>/g;
